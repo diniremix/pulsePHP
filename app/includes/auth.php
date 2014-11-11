73029fe874253651e1031de9981a330056dd5d22
Jorge Brunal <diniremix@gmail.com>
2014-11-10 23:55:56 -0500

    creadas funciones para el manejo de la tabla users, modificaciones a la documentacion de las funciones

--- app/includes/auth.php
+++ app/includes/auth.php
@@ -1,12 +1,12 @@
 <?php
 
-class Auth{
+class Auth extends Database{
  
     // blowfish
     private static $algo = '$2a';
     // cost parameter
     private static $cost = '$10';
- 
+
     // mainly for internal use
     public static function unique_salt() {
         return substr(sha1(mt_rand()), 0, 22);
@@ -37,7 +37,7 @@ class Auth{
     /**
      * [generatePasswordHash generating random Unique SHA1 String for password ]
      * @param  [array] $fields [password to generate]
-     * @return [string]         [sha1 hash]
+     * @return [string] hash [sha1 hash]
      */
     public static function generatePasswordHash($fields) {
         $password=$fields['password'];
@@ -49,7 +49,7 @@ class Auth{
     /**
      * [getPasswordHash get password hash and generate a new api key]
      * @param  [type] $fields [array]
-     * @return [array]
+     * @return [array] $validKey [array with passwords hash]
      */
     public static function getUserKey($fields){
         $password=self::generatePasswordHash($fields);
@@ -96,22 +96,34 @@ class Auth{
             return FALSE;
         }
     }
-   
+    
+    /**
+     * [getApiKey Validating if user api key exists]
+     * @param  [String]  $api_key [user api key]
+     * @return [array] $userID [user id]
+     */
+    private static function getApiKey($api_key) {
+        $userID=self::execQuery( 'SELECT id from users WHERE api_key = ?', array($api_key));
+        if($userID){
+            return $userID;
+        }else{
+            return NULL;
+        }
+    }
+    
     /**
      * [isValidApiKey Validating user api key
      * If the api key is there in db, it is a valid key]
      * @param  [String]  $api_key [user api key]
-     * @return boolean          [description]
+     * @return boolean [true or false if is a valid key]
      */
     public static function isValidApiKey($api_key) {
-        $bc= new baseController();
-        $isValid=$bc->execQuery( 'SELECT id from users WHERE api_key = ?', array($api_key));
+        $isValid=self::getApiKey($api_key);
         if($isValid){
             return true;
         }else{
             return false;
         }
-        //return $isValid > 0;
     }
     
     /**
@@ -120,19 +132,57 @@ class Auth{
      * @return [string] $user_id [user ID]
      */
     public static function getUserId($api_key) {
-        $bc= new baseController();
-        $user_id=$bc->execQuery( 'SELECT id from users WHERE api_key = ?', array($api_key));
+        $user_id=self::getApiKey($api_key);
         if($user_id){
             return $user_id;
         }else{
             return NULL;
         }
     }
+
+    /**
+     * Fetching user by email or username
+     * @param String $fields User email or username id
+     * @return [array] $user [array fields from user]
+     */
+    public static function getUserById($fields) {
+        $field=filter_var($fields, FILTER_VALIDATE_EMAIL);
+        if($field){
+            $cond="email=";
+        }else{
+            $cond="username=";
+        }
+        $cond.="'".$fields."' LIMIT 1";
+
+        $user=R::getRow('SELECT name, username, email, api_key, status, created_at FROM users WHERE '.$cond);
+        if ($user){
+            return $user;
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Checking for duplicate user by email address
+     * @param String $email email to check in db
+     * @return boolean
+     */
+    public static function isUserExists($email) {
+        $UserExist=self::execQuery( 'SELECT id from users WHERE email = ?', array($email));
+        if ($UserExist) {
+            return $UserExist;
+        }else{
+            return NULL;
+        }
+
+    }
 }//class
 
 /**
  * Adding Middle Layer to authenticate every request
  * Checking if the request has valid api key in the 'Authorization' header
+ * @param  SlimRoute $route [Slim route]
+ * @return [string] $user_id [a valid user ID]
  */
 function authenticate(\Slim\Route $route) {
     // Getting request headers
