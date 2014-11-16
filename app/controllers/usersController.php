a401bc4391884aaa8026aa3f34890cede71322a6
Jorge Brunal <diniremix@gmail.com>
2014-11-16 07:25:44 -0500

    creacion del controlador para la clase users

--- app/controllers/usersController.php
+++ app/controllers/usersController.php
@@ -4,5 +4,31 @@ class usersController extends Users{
 	function __construct(){
 		# code...
 	}
+
+	function getAll($app){
+        $dbo = new Database();
+        $result = $dbo->search('users');
+        if ($result != NULL) {
+            echoRespnse(0,DEFAULT_MESSAGE,$result);
+        }else{
+            echoRespnse(701,RESOURCE_NOT_EXIST);
+        }
+	}
+	
+	function getOne($app,$item_id){
+		$id = preg_replace("/[^0-9]/", "", $item_id);
+	    $id=(int)$id;
+	    if($id>0){
+	        $dbo = new Database();
+	        $result = $dbo->search('users',$id);
+	        if ($result != NULL) {
+	            echoRespnse(0,DEFAULT_MESSAGE,$result);
+	        } else {
+	            echoRespnse(701,RESOURCE_NOT_EXIST);
+	        }
+	    }else{
+	        echoRespnse(701,RESOURCE_NOT_EXIST);
+	    }
+	}
 }
 ?>
