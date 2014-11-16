1c3769637212ff5910409cdd6902a13c13f6d70d
Jorge Brunal <diniremix@gmail.com>
2014-11-16 09:59:10 -0500

    creadas las funciones para actualizar y eliminar, revisar la funcion createUser, para redirigirla a /register en lo posible

--- app/controllers/usersController.php
+++ app/controllers/usersController.php
@@ -30,5 +30,55 @@ class usersController extends Users{
 	        echoRespnse(701,RESOURCE_NOT_EXIST);
 	    }
 	}
+
+	function createUser($app){
+		//vease '/register'
+		$app->redirect('register');
+	}
+
+	function updateUser($app,$item_id){
+		//except the password
+		$id = preg_replace("/[^0-9]/", "", $item_id);
+	    $id=(int)$id;
+	    if($id>0){
+		    $upd_user=verifyRequiredParams(array('name', 'username','email','status'));
+	        // validating email address
+	        validateEmail($upd_user['email']);
+	        if (Auth::isUserExists($id)){
+			    //add id of the user
+			    $upd_user=array_merge($upd_user,array('id'=>$id));
+			    // updating user
+	        	$dbo = new Database();
+			    $result = $dbo->update('users',$upd_user);
+			    if ($result!=NULL) {
+			        echoRespnse(0,'User updated successfully',$result);
+			    }else{
+			        // user failed to update
+			        echoRespnse(1002,'User failed to update. Please try again!');
+			    }
+	        }else{
+	            // User with the email not existed in the db
+	            echoRespnse(900,'The requested User doesn\'t exists');
+	        }
+	    }else{
+	        echoRespnse(701,RESOURCE_NOT_EXIST);
+	    }
+	}
+
+	function deleteUser($app,$item_id){
+	    $id = preg_replace("/[^0-9]/", "", $item_id);
+	    $id=(int)$id;
+	    if($id>0){
+	    	$dbo = new Database();
+		    $result = $dbo->delete('users',$id);
+		    if ($result!=NULL) {
+		        echoRespnse(0,DEFAULT_MESSAGE,$result);
+		    } else {
+		        echoRespnse(1002,QUERY_FAILED,$result);
+		    }
+		}else{
+	        echoRespnse(701,RESOURCE_NOT_EXIST);
+	    }
+	}
 }
 ?>
