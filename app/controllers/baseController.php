63977ed3c0c1c469846490bfb0c100f07ec7eaa1
Jorge Brunal <diniremix@gmail.com>
2014-11-16 07:15:28 -0500

    correcciones varias, eliminacion de variables y comentarios

--- app/controllers/baseController.php
+++ app/controllers/baseController.php
@@ -1,10 +1,8 @@
 <?php 
-//class baseController extends Database{
 class baseController {
 	function __construct(){
 		require_once (APP_ABSPATH.'../includes/database.php');
-        //require_once (APP_ABSPATH.'../includes/PassHash.php');
-        $db= Database::init();
+        Database::init();
 	}
 
 	function createIntance($class,$app){
@@ -17,8 +15,6 @@ class baseController {
 			include_once($controller);
 			$newClass= "{$controllerName}";
 			if (class_exists($newClass)) {
-			    //$objClass = new $dnewClass();
-				//return $objClass;
 				return $newClass;
 			}else{
 				return NULL;
@@ -29,23 +25,12 @@ class baseController {
 		}
 	}
 
-	function getPropertiesClass(/*$class*/){
+	function getPropertiesClass(){
 		$properties =array();
-		//$class_vars=get_object_vars($this);
 		$class_vars = get_class_vars(get_class($this));
 		foreach ($class_vars as $name => $value) {
 		    array_push($properties, $name);
 		}
-		/* 
-		using ReflectionClass
-		$reflect = new ReflectionClass($this);
-		$props   = $reflect->getProperties(ReflectionProperty::IS_PUBLIC ||ReflectionProperty::IS_PRIVATE||ReflectionProperty::IS_PROTECTED);
-		
-		foreach ($props as $prop) {
-			$prop->setAccessible(true);
-		    array_push($properties, $prop->getName());
-		}
-		*/
 		return $properties;
 	}
 }
