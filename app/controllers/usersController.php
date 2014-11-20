4d87cd8e932ab58f352679340c8a5536360ddc83
Jorge Brunal <diniremix@gmail.com>
2014-11-20 12:05:14 -0500

    la funcion getOne permite realizar consulta por ID de usuario o por username

--- app/controllers/usersController.php
+++ app/controllers/usersController.php
@@ -16,19 +16,26 @@ class usersController extends Users{
 	}
 	
 	function getOne($app,$item_id){
-		$id = preg_replace("/[^0-9]/", "", $item_id);
-	    $id=(int)$id;
-	    if($id>0){
-	        $dbo = new Database();
-	        $result = $dbo->search('users',$id);
-	        if ($result != NULL) {
-	            echoRespnse(0,DEFAULT_MESSAGE,$result);
-	        } else {
-	            echoRespnse(701,RESOURCE_NOT_EXIST);
-	        }
-	    }else{
-	        echoRespnse(701,RESOURCE_NOT_EXIST);
-	    }
+		$item=sanityCheck($item_id);
+		$id=(int)$item;
+		
+		$dbo = new Database();
+		if($id>0){
+			$result = $dbo->search('users',$id);
+			if ($result != NULL) {
+				echoRespnse(0,DEFAULT_MESSAGE,$result);
+			} else {
+				echoRespnse(701,RESOURCE_NOT_EXIST);
+			}
+		}else{
+			$cond="username='".$item."'";
+			$result=$dbo->select(array('users'),NULL,$cond);
+			if ($result != NULL) {
+				echoRespnse(0,DEFAULT_MESSAGE,$result);
+			} else {
+				echoRespnse(701,RESOURCE_NOT_EXIST);
+			}
+		}
 	}
 
 	function createUser($app){
