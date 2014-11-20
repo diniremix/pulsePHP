21bddbaeef239c295459932cca219e00ccf3b9dd
Jorge Brunal <diniremix@gmail.com>
2014-11-20 11:05:57 -0500

    creadas constantes para la base de datos, directorio de Routes

--- app/config/Config.php
+++ app/config/Config.php
@@ -7,13 +7,15 @@ define('DB_NAME', 'taskfm');
 
 define('DB_PORT', '3306');
 define('DB_DEFAULT', 'mysql');
+
 /*=========== App Configuration ==========*/
 define('APP_ERROR','Application Error');
 define('APP_MSG','Application Message');
 define('APP_STAT','Application Status');
+define('ROUTES_APP','../routes/');
 define('API_NAME','PulsePHP');
 define('USE_API','/v1');
-define('API_VERSION','v2.1.1');
+define('API_VERSION','v2.1.3');
 define('API_FULLNAME',API_NAME.' '.API_VERSION);
 //define('PREFIX_FUNCTION','fn_');
 
@@ -23,10 +25,16 @@ define('CONTENT_PDF','application/pdf');
 define('CONTENT_XML','application/xml');
 define('APP_TYPE_CONTENT_DEFAULT',CONTENT_JSON);
 
+/*=========== Database messages ==========*/
+define('DATABASE_DRIVER_NOT_ALLOWED', 'Error: Database driver not allowed');
+define('MISSING_ARRAY_FIELDS', 'Array field(s) is missing');
+define('MISSING_ARRAY_TABLENAMES', 'Array table name(s) is missing');
+define('MISSING_PORT_NUMBER', 'Cubrid: Port number is required');
+
 /*=========== messages ==========*/
 define('EXPIRED_API_KEY', 'Access Denied. Api key is expired');
 define('INVALID_API_KEY', 'Access Denied. Invalid Api key');
-define('MISSSING_API_KEY', 'Api key is misssing');
+define('MISSING_API_KEY', 'Api key is missing');
 
 define('USER_CREATED_SUCCESSFULLY', 'User created successfully');
 define('USER_CREATE_FAILED', 'User create failed');
