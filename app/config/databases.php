d03f20c786567ddc9ef93c0165587d70715c8c38
Jorge Brunal <diniremix@gmail.com>
2015-01-30 20:05:49 -0500

    agregado archivo de configuracion para las bases de datos a usar en la app

---
+++ app/config/databases.php
@@ -0,0 +1,31 @@
+<?php
+	$databases = array(
+		'DB_DEFAULT' => 'mysql',
+
+		'sqlite' => array(
+			'DB_NAME' => '',
+		),
+
+		'mysql' => array(
+			'DB_USERNAME' => '',
+			'DB_PASSWORD' => '',
+			'DB_HOST' => '',
+			'DB_NAME' => '',
+		),
+
+		'postgresql' => array(
+			'DB_USERNAME' => '',
+			'DB_PASSWORD' => '',
+			'DB_HOST' => '',
+			'DB_NAME' => '',
+		),
+
+		'cubrid' => array(
+			'DB_USERNAME' => '',
+			'DB_PASSWORD' => '',
+			'DB_HOST' => '',
+			'DB_NAME' => '',
+			'DB_PORT' => ''
+		),
+	);
+?>
