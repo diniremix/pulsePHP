5bcba6a91f938b24718ae394956cefd765054c30
Jorge Brunal <diniremix@gmail.com>
2015-02-02 12:49:18 -0500

    agregado ejemplos de uso, por defecto se usa "none" para "DB_DEFAULT"

--- app/config/databases.php
+++ app/config/databases.php
@@ -1,6 +1,24 @@
 <?php
+/*=========== Route functions for user class ==========*/
+/*This file is part of the PulsePHP, Be careful with this file */
+
+/*
+examples:
+some values for DB_DEFAULT:
++ none
++ mysql
++ postgresql
++ sqlite
++ cubrid
+
+- none: Not using a Database
+- mysql and postgresql use the same settings
+- sqlite: not supported using ":memory:" in this version
+- cubrid: DB_PORT is required
+*/
+
 	$databases = array(
-		'DB_DEFAULT' => 'mysql',
+		'DB_DEFAULT' => 'none',
 
 		'sqlite' => array(
 			'DB_NAME' => '',
