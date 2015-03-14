00dba1e1030cba2aaa4c9c600a29f02b73501914
Jorge Brunal <diniremix@gmail.com>
2015-03-14 00:27:29 -0500

    el routing del index ahora sera gestionado por application.php

--- index.php
+++ index.php
@@ -2,24 +2,6 @@
 define('APP_ABSPATH', dirname(__FILE__).'/app/');
 require_once(APP_ABSPATH.'config/app.php');
 require_once(APP_ABSPATH.'route.php');
-/*====================== App Route Configuration =====================*/
-
-$app->group('/', function () use ($app) {
-    $app->map('/',  function()  use ($app){
-    	echoRespnse(0, 'Aloha! from '.API_FULLNAME.' please using '.API_NAME.USE_API.' instead root');
-    })->via('GET', 'POST');
-});
-
-$app->group(USE_API, function () use ($app) {
-    $app->map('/',  function()  use ($app){
-		echoRespnse(0, 'Aloha! from '.API_FULLNAME);
-	})->via('GET', 'POST');
-});
-
-$app->notFound(function () use ($app) {
-    //$app->render('404.html');
-    echoRespnse(702, NOT_FOUND);
-});
 
 //run the app
 $app->run();
