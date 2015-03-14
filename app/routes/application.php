0d3328035325802a6a012c6c24f100250bed8606
Jorge Brunal <diniremix@gmail.com>
2015-03-14 00:34:45 -0500

    las funciones de /login y /register van a otro archivo, en application, se establece routes internos de la api de pulsePHP

--- app/routes/application.php
+++ app/routes/application.php
@@ -2,56 +2,27 @@
 /*=========== App Route functions ==========*/
 /*This file is part of the PulsePHP, Be careful with this file */
 
-$app->group(USE_API, function () use ($app) {
-    
-    $app->post('/login', function() use ($app) {
-        // check for required params
-        $fields = array('username', 'password');
-        $formdata=verifyRequiredParams($fields);
+$app->group('/', function () use ($app) {
+    $app->map('/', function() use ($app){
+        echoRespnse(0, 'Aloha! from '.API_FULLNAME.' please using '.'/api'.USE_API.' instead root');
+    })->via('GET', 'POST');
+});
 
-        if (Auth::checkLogin($formdata)){
-            $user = Auth::getUserById($formdata['username']);
-            if (!is_null($user)){
-                echoRespnse(0,DEFAULT_MESSAGE,$user);
-            }else{
-                // unknown error occurred
-                echoRespnse(1004,DEFAULT_ERROR_MESSAGE);
-            }
-        }else{
-            // user credentials are wrong
-            echoRespnse(1001,LOGIN_FAILED);
-        }
+$app->group('/api', function () use ($app) {
+    $app->map('/', function() use ($app){
+         echoRespnse(0, 'Aloha! from '.API_FULLNAME.' current version api: '.USE_API);
+    })->via('GET', 'POST');
+
+    $app->group(USE_API, function () use ($app) {
+        $app->map('/',  function()  use ($app){
+            echoRespnse(0, 'This is the version "'.USE_API.'" of '.API_FULLNAME.', for more info visit: '.GITHUB_URL_APP);
+        })->via('GET', 'POST');
     });
+});
 
-    $app->post('/register', function() use ($app) {
-        global $bc;
-        $users= $bc->createIntance('users',$app);
-        $user = new $users();
-        $fields = $user->getPropertiesClass();
-        
-        // check for required params
-        $formdata=verifyRequiredParams($fields);
-        // validating email address
-        validateEmail($formdata['email']);
-
-        if (!Auth::isUserExists($formdata['email'])) {
-            //get password hash and generate api key
-            $key=Auth::getUserKey($formdata);
-            $formdata=array_merge($formdata,$key);
-            //save form
-            $dbo = new Database();
-            $result=$dbo->save('users',$formdata);
-            
-            if ($result != NULL) {
-                echoRespnse(0,USER_CREATED_SUCCESSFULLY,$result);
-            }else{
-                echoRespnse(901,USER_CREATE_FAILED);
-            }
-        }else{
-            // User with same email already existed in the db
-            echoRespnse(900,USER_ALREADY_EXISTED);
-        }
-    });
+$app->notFound(function () use ($app) {
+    //$app->render('404.html');
+    echoRespnse(702, NOT_FOUND);
 });
 
 ?>
