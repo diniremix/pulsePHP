d948639f259e6ef0e4bea73f073476e5a97ab865
Jorge Brunal <diniremix@gmail.com>
2015-03-23 23:30:42 -0500

    se corrige la funcion register, creando una clase Persons, para el manejo ma completo del registro de usuarios

--- app/routes/login.php
+++ app/routes/login.php
@@ -27,27 +27,45 @@ $app->group('/api', function () use ($app) {
 
         $app->post('/register', function() use ($app) {
             global $bc;
-            $users= $bc->createIntance('users',$app);
-            $user = new $users();
-            $fields = $user->getPropertiesClass();
-            
-            // check for required params
-            $formdata=verifyRequiredParams($fields);
-            // validating email address
-            validateEmail($formdata['email']);
+            $Users= $bc->createIntance('Users',$app);
+            $user = new $Users();
+            $fields_u = $user->getPropertiesClass();
 
-            if (!Auth::isUserExists($formdata['email'])) {
-                //get password hash and generate api key
-                $key=Auth::getUserKey($formdata);
-                $formdata=array_merge($formdata,$key);
+            $Persons= $bc->createIntance('Persons',$app);
+            $person = new $Persons();
+            $fields_p = $person->getPropertiesClass();
+
+            // check for required params
+            $formDataU=verifyRequiredParams($fields_u);
+            $formDataP=verifyRequiredParams($fields_p);
+
+            // validating email address
+            validateEmail($formDataP['email']);
+
+            if (!Auth::isUserExists($formDataP['email'])) {
                 //save form
                 $dbo = new Database();
-                $result=$dbo->save('users',$formdata);
-                
-                if ($result != NULL) {
-                    echoRespnse(0,USER_CREATED_SUCCESSFULLY,$result);
+                $resultP=$dbo->save('persons',$formDataP);
+                if ($resultP != NULL) {
+                    //get password hash and generate api key
+                    $keypass=Auth::getUserKey($formDataU);
+                    $formDataU=array_merge($formDataU,$keypass);
+
+                    //verify if person_id and role_id from formdata
+                    $personId=array('person_id'=>$resultP);
+                    $formDataU=array_merge($formDataU,$personId);
+                    $roleId=array('role_id'=>2);
+                    $formDataU=array_merge($formDataU,$roleId);
+
+                    $resultU=$dbo->save('users',$formDataU);
+
+                    if ($resultU != NULL) {
+                        echoRespnse(0,USER_CREATED_SUCCESSFULLY);
+                    }else{
+                        echoRespnse(901,USER_CREATE_FAILED,$resultU);
+                    }
                 }else{
-                    echoRespnse(901,USER_CREATE_FAILED);
+                    echoRespnse(901,USER_CREATE_FAILED,$resultP);
                 }
             }else{
                 // User with same email already existed in the db
