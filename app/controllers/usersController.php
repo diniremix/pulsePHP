6584bcd1e6db02d4315760b428fc9fc8915923de
Jorge Brunal <diniremix@gmail.com>
2015-03-26 10:54:17 -0500

    eliminada la funcion "updateUser", se usa "/register" en su reemplazo, correcciones menores

--- app/controllers/usersController.php
+++ app/controllers/usersController.php
@@ -5,9 +5,9 @@ class usersController extends Users{
 		# code...
 	}
 
-	function getAll($app){
+	function getAll($app,$table){
         $dbo = new Database();
-        $result = $dbo->search('users');
+        $result = $dbo->search($table);
         if ($result != NULL) {
             echoRespnse(0,DEFAULT_MESSAGE,$result);
         }else{
@@ -36,40 +36,6 @@ class usersController extends Users{
 				echoRespnse(701,RESOURCE_NOT_EXIST);
 			}
 		}
-	}
-
-	function createUser($app){
-		//vease '/register'
-		$app->redirect('register');
-	}
-
-	function updateUser($app,$item_id){
-		//except the password
-		$id = preg_replace("/[^0-9]/", "", $item_id);
-	    $id=(int)$id;
-	    if($id>0){
-		    $upd_user=verifyRequiredParams(array('name', 'username','email','status'));
-	        // validating email address
-	        validateEmail($upd_user['email']);
-	        if (Auth::isUserExists($id)){
-			    //add id of the user
-			    $upd_user=array_merge($upd_user,array('id'=>$id));
-			    // updating user
-	        	$dbo = new Database();
-			    $result = $dbo->update('users',$upd_user);
-			    if ($result!=NULL) {
-			        echoRespnse(0,'User updated successfully',$result);
-			    }else{
-			        // user failed to update
-			        echoRespnse(1002,'User failed to update. Please try again!');
-			    }
-	        }else{
-	            // User with the email not existed in the db
-	            echoRespnse(900,'The requested User doesn\'t exists');
-	        }
-	    }else{
-	        echoRespnse(701,RESOURCE_NOT_EXIST);
-	    }
 	}
 
 	function deleteUser($app,$item_id){
