b726c06cf61633f602acecce29e7a05915159ee3
Peter Anderson <diniremix@gmail.com>
2016-08-04 23:41:56 -0500

    fix errors route, use namespace Rest, and Errors, $app works if getInstance()

--- app/routes/errors.php
+++ app/routes/errors.php
@@ -1,26 +1,20 @@
 <?php
 /*=========== Route functions for Error codes ==========*/
 /*This file is part of the PulsePHP, Be careful with this file */
+use app\libraries\Rest;
+use app\libraries\Errors;
+$app = \Slim\Slim::getInstance();
 
-$app->group('/api', function () use ($app) {
-	$app->group(USE_API, function () use ($app) {
-		
-		$app->group('/errors', function () use ($app) {
-			
-			$app->get('/', function() use ($app) {
-			    global $messages;
-			    echoRespnse(0, 'Error Codes',$messages);
-			});
+$app->group(API_NAME, function () use ($app) {
+	$app->group('/errors', function () use ($app) {
+		$app->get('/', function() use ($app) {
+			$messages = Errors::allErrors();
+		    Rest::response(200, 'Error Codes',$messages);
+		});
 
-			$app->get('/:id', function($error_id) {
-			    global $messages;
-			    if(array_key_exists($error_id, $messages)){
-			        echoRespnse(0, 'Error Code: '.$error_id,$messages[$error_id]);
-			    }else{
-			        echoRespnse(1100,'Invalid code Error');
-			    }
-			});
-
+		$app->get('/:id', function($error_id) {
+			$message = Errors::getOne($error_id);
+			Rest::response(200, $message);
 		});
 	});
 });
