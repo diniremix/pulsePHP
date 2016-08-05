4db1c334587f8dd3bd04ce7ec3953d51515b3e94
Peter Anderson <diniremix@gmail.com>
2016-08-04 23:38:49 -0500

    module for dynamic load modules and routes

---
+++ app/libraries/modules.php
@@ -0,0 +1,93 @@
+<?php
+namespace app\libraries;
+class Modules {
+
+    private static $_modules =array(
+        'config' => array(
+            'constants',
+            'app',
+            'databases',
+        ),
+        'paths' => array(
+            'CONFIG_PATH'=> 'config',
+            'LIBRARIES_PATH'=> 'libraries',
+            'ROUTES_PATH'=> 'routes',
+            'TEMPLATES_PATH'=> 'templates',
+            'STORAGE_PATH'=> 'storage',
+            'VENDOR_PATH'=> 'vendor',
+            'SCHEME'=> 'scheme',
+        ),
+        'libraries' => array(
+            //'databases',
+            //'basicAuth',
+            'errorCodes',
+            //'pulseAcl',
+            //'pulseLog',
+            'rest',
+            'sessions',
+            'utils',
+
+            //'socket',
+            //'network',
+        ),
+        'vendor' => array(
+            'Slim' => 'Slim-2.6.2/Slim/Slim.php',
+            //'redbean' => 'redbean/rb.php',
+        ),
+    );
+
+    public static function registerModules(){
+        $_config=self::$_modules['paths'];
+        $_vendors=self::$_modules['vendor'];
+        $_apps=self::$_modules['config'];
+        $_libraries=self::$_modules['libraries'];
+
+        foreach ($_vendors as $vendor) {
+            $modfile = APP_ABSPATH.DIRECTORY_SEPARATOR.$_config['VENDOR_PATH'].DIRECTORY_SEPARATOR.$vendor;
+            if(file_exists($modfile)){
+                if(is_readable($modfile)){
+                    require_once $modfile;
+                }
+            }
+        }
+
+        foreach ($_apps as $app) {
+            $modfile = APP_ABSPATH.DIRECTORY_SEPARATOR.$_config['CONFIG_PATH'].DIRECTORY_SEPARATOR.$app.'.php';
+            if(file_exists($modfile)){
+                if(is_readable($modfile)){
+                    require_once $modfile;
+                }
+            }
+        }
+
+        foreach ($_libraries as $module) {
+            $modfile = APP_ABSPATH.DIRECTORY_SEPARATOR.$_config['LIBRARIES_PATH'].DIRECTORY_SEPARATOR.$module.'.php';
+            if(file_exists($modfile)){
+                if(is_readable($modfile)){
+                    require_once $modfile;
+                }
+            }
+        }
+    }
+
+    public static function loadRoutes(){
+        $_routesDir=APP_ABSPATH.DIRECTORY_SEPARATOR.self::$_modules['paths']['ROUTES_PATH'];
+        if($dh = opendir($_routesDir)){
+            while(($file = readdir($dh)) !== false){
+                $modroute=$_routesDir.DIRECTORY_SEPARATOR.$file;
+                if(file_exists($modroute)){
+                    if (preg_match('/php/i', $modroute)){
+                        if(is_readable($modroute)){
+                            if (($file != ".") && ($file != "..")){
+                                require_once $modroute;
+                            }
+                        }
+                    }
+                }
+            }
+            closedir($dh);
+        }
+    }
+}
+
+?>
