e6ebf59ce99a720474d54bc5c230c49c50fb88fe
Jorge Brunal <jorge.brunal@gmail.com>
2017-09-04 23:32:32 -0500

    create a Utils class, move some methods to Rest, loadRoutes and loadRoutesOnDemand functions are deprecated

--- app/libraries/utils.php
+++ app/libraries/utils.php
@@ -1,110 +1,48 @@
 <?php
-/**
- * [isJSONData to evaluate for a valid JSON]
- * @param  [JSON]  $jsonData [JSON to validate]
- * @return [a valid JSON or application error]
- */
-function isJSONData($jsonData){
-    $decodedJson = json_decode(stripslashes($jsonData), TRUE);
-    if(json_last_error() == JSON_ERROR_NONE){
-        return $decodedJson;
-    }else{
+namespace app\libraries;
+class Utils {
+
+    /**
+     * Validating email address
+     */
+    /**
+     * [validateEmail Validating email address]
+     * @param  [type]  $email
+     * @param  boolean $stopApp [defines whether the application will be stopped]
+     * @return [boolean]
+     */
+    public static function validateEmail($email) {
         $app = \Slim\Slim::getInstance();
-        echoRespnse(1005, "Invalid JSON value found",$decodedJson);
-        $app->stop();
-    }
-}
-
-/**
- * Verifying required params posted or not
- */
-function verifyRequiredParams($required_fields) {
-    $error = false;
-    $error_fields = "";
-    $request_params = array();
-    $request_params = $_REQUEST;
-
-    $app = \Slim\Slim::getInstance();
-    $req= $app->request()->getBody();
-    $request_params= isJSONData($req);
-
-    if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
-        $app = \Slim\Slim::getInstance();
-        parse_str($app->request()->getBody(), $request_params);
-            $request_params=isJSONData($request_params);
-    }
-
-    foreach ($required_fields as $field) {
-        if (!isset($request_params[$field]) || strlen(trim($request_params[$field])) <= 0) {
-            $error = true;
-            $error_fields .= $field . ', ';
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echoRespnse(1003, EMAIL_FAILURE);
+            $app->stop();
+        }else{
+            return true;
         }
     }
 
-    if (!$error){
-        foreach ($required_fields as $field){
-            if (is_null(sanityCheck($request_params[$field]))){
-                $error = true;
-                $error_fields .= $field . ', ';
-            }
+    /**
+     * clean data from forms
+     */
+    public static function sanityCheck($values){
+        $strip = strip_tags($values);
+        $htmlentities = htmlentities($strip);
+        if(empty($htmlentities)){
+            return NULL;
         }
+        return $htmlentities;
     }
 
-    if ($error){
-        // Required field(s) are missing or empty
-        // echo error json and stop the app
-        $app = \Slim\Slim::getInstance();
-        echoRespnse(1005, 'Required field(s) "' . substr($error_fields, 0, -2) . '" is missing or empty');
-        $app->stop();
-    }else{
-        //return form values
-        $formdata=array();
-        foreach ($required_fields as $value) {
-            $formdata[$value] = $request_params[$value];
-        }
-        return $formdata;
+    /**
+     * [gettimestamp generates a timestamp based on the date and time of the server]
+     * @return [sring] [a timestamp]
+     */
+    public static function getTimeStamp(){
+        return date("YmdHis");//ej: 20100525151036
     }
-}
+}//Utils
 
-/**
- * Validating email address
- */
-/**
- * [validateEmail Validating email address]
- * @param  [type]  $email
- * @param  boolean $stopApp [defines whether the application will be stopped]
- * @return [boolean]
- */
-function validateEmail($email) {
-    $app = \Slim\Slim::getInstance();
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echoRespnse(1003, EMAIL_FAILURE);
-        $app->stop();
-    }else{
-        return true;
-    }
-}
-
-/**
- * clean data from forms
- */
-function sanityCheck($values){
-	$strip = strip_tags($values);
-	$htmlentities = htmlentities($strip);
-    if(empty($htmlentities)){
-        return NULL;
-    }
-	return $htmlentities;
-}
-
-/**
- * [gettimestamp generates a timestamp based on the date and time of the server]
- * @return [sring] [a timestamp]
- */
-function getTimeStamp(){
-    return date("YmdHis");//ej: 20100525151036
-}
-
+/*******************************************************************************/
 /**
  * Echoing json response to client
  * @param Int $errorCode response code error
@@ -140,6 +78,7 @@ function echoRespnse($errorCode,$message,$data=NULL) {
 }
 
 /**
+ * DEPRECATED
  * [loadRoutes search for routes modules]
  * @return [array] [routes]
  */
@@ -162,6 +101,7 @@ function loadRoutes(){
 }
 
 /**
+ * DEPRECATED
  * [loadRoutesOnDemand search for custom modules]
  * @param  [array] $routesFiles [modules array]
  * @return [array]              [modules array paths]
