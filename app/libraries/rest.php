2c7c077b949ecceba8ac19af07861a86fb509be6
Jorge Brunal <jorge.brunal@gmail.com>
2017-09-04 23:36:07 -0500

    move some functions from Utils, fix docs

--- app/libraries/rest.php
+++ app/libraries/rest.php
@@ -1,9 +1,28 @@
 <?php
 namespace app\libraries;
+use app\libraries\Utils;
 class Rest {
 
     /**
-     * Verifying required params posted or not
+     * [isJSONData to evaluate for a valid JSON]
+     * @param  [JSON]  $jsonData [JSON to validate]
+     * @return [a valid JSON or application error]
+     */
+    public static function isJSONData($jsonData){
+        $decodedJson = json_decode(stripslashes($jsonData), TRUE);
+        if(json_last_error() == JSON_ERROR_NONE){
+            return $decodedJson;
+        }else{
+            $app = \Slim\Slim::getInstance();
+            self::response(1005, "Invalid JSON value found",$decodedJson);
+            $app->stop();
+        }
+    }
+
+    /**
+     * [Verifying required params posted or not]
+     * @param  [array] $required_fields [fields to check]
+     * @return [valid array fields or application error]
      */
     public static function verifyRequiredParams($required_fields) {
         $error = false;
@@ -11,17 +30,14 @@ class Rest {
         $request_params = array();
         $request_params = $_REQUEST;
 
-        if(isset($_REQUEST['jsonData'])){
-            $request_params=isJSONData($_REQUEST['jsonData']);
-        }else{
-            // Handling PUT request params
-            if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
-                $app = \Slim\Slim::getInstance();
-                parse_str($app->request()->getBody(), $request_params);
-                if(isset($request_params['jsonData'])){
-                    $request_params=isJSONData($request_params['jsonData']);
-                }
-            }
+        $app = \Slim\Slim::getInstance();
+        $req= $app->request()->getBody();
+        $request_params= self::isJSONData($req);
+
+        if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
+            $app = \Slim\Slim::getInstance();
+            parse_str($app->request()->getBody(), $request_params);
+                $request_params=self::isJSONData($request_params);
         }
 
         foreach ($required_fields as $field) {
@@ -33,7 +49,7 @@ class Rest {
 
         if (!$error){
             foreach ($required_fields as $field){
-                if (is_null(sanityCheck($request_params[$field]))){
+                if (is_null(Utils::sanityCheck($request_params[$field]))){
                     $error = true;
                     $error_fields .= $field . ', ';
                 }
@@ -44,7 +60,7 @@ class Rest {
             // Required field(s) are missing or empty
             // echo error json and stop the app
             $app = \Slim\Slim::getInstance();
-            echoRespnse(1005, 'Required field(s) "' . substr($error_fields, 0, -2) . '" is missing or empty');
+            self::response(1005, 'Required field(s) "' . substr($error_fields, 0, -2) . '" is missing or empty');
             $app->stop();
         }else{
             //return form values
@@ -57,22 +73,11 @@ class Rest {
     }
 
     /**
-     * clean data from forms
-     */
-    public static function sanityCheck($values){
-    	$strip = strip_tags($values);
-    	$htmlentities = htmlentities($strip);
-        if(empty($htmlentities)){
-            return NULL;
-        }
-    	return $htmlentities;
-    }
-
-    /**
      * Echoing json response to client
      * @param Int $errorCode response code error
      * @param String $message response message title
      * @param Array $data Json response
+     * @return [a json response]
      */
     public static function response($statusCode,$message,$data=NULL) {
         $status_code=200;
@@ -101,4 +106,4 @@ class Rest {
         $app->contentType(APP_TYPE_CONTENT_DEFAULT);
         echo json_encode($response);
     }
-}//Utils
+}//Rest
